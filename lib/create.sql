c33c44c9872c515af9cdc11ac9c861de1d089ff0
Abed Nganga <ngangaabed@gmail.com>
2023-08-26 14:45:21 +0300

    Completed Assignment

---
+++ lib/create.sql
@@ -0,0 +1,9 @@
+CREATE TABLE bears (
+    id integer,
+    name tex,
+    age integer,
+    sex text,
+    color text,
+    temperament text,
+    alive boolean
+) 
---
+++ lib/insert.sql
@@ -0,0 +1,10 @@
+INSERT INTO bears (name,age,sex,color,temperament,alive) 
+VALUES
+("Mr. Chocolate", 1, "M","white","calm",1),
+("Rowdy", 2, "F","Grey","wild",1),
+("Tabitha", 9, "M","black","violet",1),
+("Sergeant Brown", 8, "M","violet","calm",0),
+("Melissa", 11, "F","purple","feisty",1),
+("Grinch", 1, "F","blue","groggy",1),
+("Wendy", 3, "M","zebra","sums",1),
+(NULL, 4, "F","white","fluffs",1);
